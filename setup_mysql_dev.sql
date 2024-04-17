d8e35fdb436d18fa060b29e622432f536df8a6d7
akoredera <akoredera@gmail.com>
2024-04-17 14:36:47 -0700

    update setup test

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -10,3 +10,14 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- Grant SELECT privilege on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
+-- Create the user if not exists
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Create the database if not exists
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Grant privileges to the user on the database
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
