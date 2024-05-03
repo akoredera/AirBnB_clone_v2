268f8a4d1300d32707c039e9a2ce8addacbaf6c6
akoredera <akoredera@gmail.com>
2024-05-03 14:05:54 -0700

    modified task 0 file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,8 +3,12 @@
 sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-echo "<html>
+sudo echo "<html>
   <head>
   </head>
   <body>
@@ -13,5 +17,5 @@ echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sudo sed -i 's,root \/var\/www\/html;,# root \/var\/www\/html;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t},' /etc/nginx/sites-enabled/default
+sudo sed -i 's/listen 80 default_server/a \n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t},' /etc/nginx/sites-enabled/default
 sudo service nginx restart
