5be82ff57adeca349887c26e6c556a5b873fff04
akoredera <akoredera@gmail.com>
2024-05-03 15:28:33 -0700

    modified file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from fabric.api import local
+from time import strftime
 from datetime import datetime
 
 
@@ -9,7 +10,7 @@ def do_pack():
     of the web_static folder of AirBnB Clone
     '''
     current_datetime = datetime.now()
-    formatted_datetime = current_datetime.strftime("%Y%m%d%H%M%S")
+    formatted_datetime = strftime("%Y%m%d%H%M%S")
     try:
         local('mkdir -p /root/AirBnB_clone_v2/versions')
         result = local('tar -cvzf versions/web_static_{}.tgz web_static/'
