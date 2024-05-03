6b96405fd006eba83bd46d238f74042875209238
akoredera <akoredera@gmail.com>
2024-05-03 14:01:22 -0700

    modified task 0 file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # Prepare your web servers
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test/
 echo "<html>
   <head>
@@ -7,7 +10,8 @@ echo "<html>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sudo sed -i 's,root \/var\/www\/html;,# root \/var\/www\/html;\n\tlocation hbnb_static {\n\t\talias /data/web_static/current/;\n\t},' /etc/nginx/sites-enabled/default
+sudo service nginx restart
