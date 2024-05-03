3688b529dfdce82b97c84a1854531d270bc9b839
akoredera <akoredera@gmail.com>
2024-05-03 14:02:58 -0700

    modified task 0 file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,5 +13,5 @@ echo "<html>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sudo sed -i 's,root \/var\/www\/html;,# root \/var\/www\/html;\n\tlocation hbnb_static {\n\t\talias /data/web_static/current/;\n\t},' /etc/nginx/sites-enabled/default
+sudo sed -i 's,root \/var\/www\/html;,# root \/var\/www\/html;\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t},' /etc/nginx/sites-enabled/default
 sudo service nginx restart
