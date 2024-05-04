175f6df766b4ced7a6baeaa4d450d634d58e39a9
akoredera <akoredera@gmail.com>
2024-05-04 06:47:13 -0700

    add task 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Full deployment
+'''
+from fabric.api import *
+from 2-do_deploy_web_static import do_deploy
+from 1-pack_web_static import do_pack
+
+env.hosts = ['34.232.67.117', '34.229.255.100']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def deploy():
+    '''
+    Fabric script (based on the file 2-do_deploy_web_static.py)
+    that creates and distributes an archive to your web servers,
+    using the function deploy
+    '''
+    output_file = do_pack()
+    if not output_file:
+        return False
+    return do_deploy(output_file)
