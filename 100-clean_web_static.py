dc0476cf3ebe571046e3d77b6c072691180e570b
akoredera <akoredera@gmail.com>
2024-05-05 06:51:10 -0700

    modified file

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,9 +3,11 @@
 Keep it clean!
 '''
 from fabric.api import *
+'''
 env.hosts = ['34.232.67.117', '34.229.255.100']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
+'''
 
 
 def do_clean(number=0):
@@ -13,9 +15,15 @@ def do_clean(number=0):
     Fabric script (based on the file 3-deploy_web_static.py)
     that deletes out-of-date archives, using the function do_clean:
     '''
-    if number == 1 or number == 0:
-        number = 1
-        with lcd('versions'):
-            local('ls -cr | head --lines -1 | xargs rm -rf')
-        with cd('/data/web_static/releases'):
-            run('ls -cr | head --lines -1 | xargs rm -rg')
+     number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
