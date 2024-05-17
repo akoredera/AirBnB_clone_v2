39a393f86caacd44c8dda639e734676ccbe07f7f
Akorede Ridwanullahi Abiodun <akoredera@gmail.com>
2024-05-17 16:17:27 +0000

    modified task 0 file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,13 +5,12 @@ Flash framework HBnB project
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_HBNB():
-	'''
-	Hello Flask!
-	'''
-	return 'Hello HBNB!'
+    '''
+    Hello Flask!
+    '''
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
